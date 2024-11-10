24d16eee67e60be503bec461911bf0554e083115
FaheemRiaz1 <faheemriaz177@gmail.com>
2024-11-10 09:58:23 +0100

    redeploy

--- notification-service/server.js
+++ notification-service/server.js
@@ -82,6 +82,7 @@ const oAuth2Client = new google.auth.OAuth2(
   process.env.CLIENT_SECRET,
   process.env.REDIRECT_URI
 )
+
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN })
 
 // Function to send an email
